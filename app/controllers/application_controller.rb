e626daf272765f49a0eec479816e97c3642461d3
kawauso <immaturer1@gmail.com>
2026-04-13 17:10:23 +0900

    ログイン・新規登録機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # ログイン失敗時などにフラッシュをTurboで即時反映させる
+  after_action :enqueue_flash_update, if: -> { response.status >= 400 }
+
+  private
+
+  def enqueue_flash_update
+    return unless Array(flash.keys).any?
+    
+    # ページ遷移なしで #flash の中身を最新のフラッシュメッセージで置き換える
+    render turbo_stream: turbo_stream.replace("flash", partial: "shared/flash")
+  end
 end
