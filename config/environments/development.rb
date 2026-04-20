57bfadfd238afc04e793fff03d122f64f4184c2b
kawauso <immaturer1@gmail.com>
2026-04-20 14:24:43 +0900

    スマホ裏返し検知エンジンおよびHTTPS通信環境の実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,14 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # 全てのホスト名/IPでのアクセスを許可
+  config.hosts.clear
+
+  # HTTPSを強制
+  config.force_ssl = true
+
+  # 127.0.0.1 や Dockerネットワーク内のプロキシからのヘッダーを信頼する
+  # Rails 7以降でNginx経由のHTTPSを正しく認識させるために必要
+  config.assume_ssl = true
 end
